59dc91ed6e252e72e8391eb5b0a51335b859b3c9
Thành <91106425+tonthatminhthanh@users.noreply.github.com>
2024-05-16 12:24:03 +0700

    Sửa đổi giao diện

--- lib/ui/widget_overlay/volume_slider.dart
+++ lib/ui/widget_overlay/volume_slider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:mw_project/objects/audio_manager.dart';
 
 class VolumeSlider extends StatefulWidget {
@@ -21,14 +22,19 @@ class _VolumeSliderState extends State<VolumeSlider> {
                     fontWeight: FontWeight.normal,
                     decoration: TextDecoration.none,
                     color: Colors.white,fontSize: 16, fontFamily: "Silver")),
-            ValueListenableBuilder(
-              valueListenable: AudioManager.getSfxVolume(),
-              builder: (context, value, child) => Slider(
-                label: value.toString(),
-                  value: value,
-                  onChanged: (value) {
-                    AudioManager.setSfxVolume(value);
-                  },
+            Center(
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.75,
+                child: ValueListenableBuilder(
+                  valueListenable: AudioManager.getSfxVolume(),
+                  builder: (context, value, child) => Slider(
+                    label: value.toString(),
+                      value: value,
+                      onChanged: (value) {
+                        AudioManager.setSfxVolume(value);
+                      },
+                  ),
+                ),
               ),
             ),
             Text("Background music volume",
@@ -36,14 +42,19 @@ class _VolumeSliderState extends State<VolumeSlider> {
                     fontWeight: FontWeight.normal,
                     decoration: TextDecoration.none,
                     color: Colors.white,fontSize: 16, fontFamily: "Silver")),
-            ValueListenableBuilder(
-              valueListenable: AudioManager.getBgmVolume(),
-              builder: (context, value, child) => Slider(
-                label: value.toString(),
-                value: value,
-                onChanged: (value) {
-                  AudioManager.setBgmVolume(value);
-                },
+            Center(
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.75,
+                child: ValueListenableBuilder(
+                  valueListenable: AudioManager.getBgmVolume(),
+                  builder: (context, value, child) => Slider(
+                    label: value.toString(),
+                    value: value,
+                    onChanged: (value) {
+                      AudioManager.setBgmVolume(value);
+                    },
+                  ),
+                ),
               ),
             ),
           ],
